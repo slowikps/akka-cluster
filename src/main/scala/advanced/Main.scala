5317fe990ab8bf996b763c68a314a2d31f1035b9
slowikps <slowikps@gmail.com>
2017-12-16 13:37:10 +0000

    One step forward, 1000 left ...

--- src/main/scala/advanced/Main.scala
+++ src/main/scala/advanced/Main.scala
@@ -1,17 +1,28 @@
 package advanced
 
-import advanced.health.ClusterStatus
-import akka.actor.ActorSystem
+import java.util.concurrent.TimeUnit
 
+import advanced.health.ClusterStatusActor
+import akka.actor.ActorSystem
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
 object Main extends App {
 
+  import akka.pattern.ask
 
   val system = ActorSystem("slowikps-system")
 
-  val healthSingletonManager = ClusterStatus.createSingletonManager(system)
-  val healthSingletonProxy = ClusterStatus.createSingletonProxy(system)
+  val healthSingletonManager = ClusterStatusActor.createSingletonManager(system)
+  val healthSingletonProxy = ClusterStatusActor.createSingletonProxy(system)
 
-  healthSingletonProxy ! ClusterStatus.PrintStatus
+  TimeUnit.MILLISECONDS.sleep(100)
 
+  implicit val timeout: Timeout = 1 second
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val status = healthSingletonProxy ? ClusterStatusActor.PrintStatus
+
+  status.map(println)
   println("Message sent")
 }
---
+++ src/main/scala/advanced/health/ClusterStatusActor.scala
@@ -0,0 +1,80 @@
+package advanced.health
+
+import advanced.health.ClusterStatusActor.PrintStatus
+import akka.actor.{Actor, ActorSystem, Address, PoisonPill, Props}
+import akka.cluster.ClusterEvent.{
+  ClusterDomainEvent,
+  InitialStateAsEvents,
+  LeaderChanged,
+  MemberUp
+}
+import akka.cluster.singleton.{
+  ClusterSingletonManager,
+  ClusterSingletonManagerSettings,
+  ClusterSingletonProxy,
+  ClusterSingletonProxySettings
+}
+import akka.cluster.{Cluster, Member}
+
+import scala.collection.mutable
+
+case class ClusterStatus(leader: Option[Address], members: Set[Member])
+
+object ClusterStatusActor {
+  private val name = "cluster-status-singleton"
+  private val managerName = s"$name-manager"
+  private val proxyName = s"$name-proxy"
+
+  private val managerPath = s"/user/$managerName"
+
+  def props: Props =
+    Props(new ClusterStatusActor)
+
+  def createSingletonManager(system: ActorSystem, props: Props = props) =
+    system.actorOf(
+      ClusterSingletonManager.props(
+        props,
+        PoisonPill,
+        ClusterSingletonManagerSettings(system)), //.withRole("not-everywhere-restriction")),
+      managerName
+    )
+
+  def createSingletonProxy(system: ActorSystem, props: Props = props) =
+    system.actorOf(
+      ClusterSingletonProxy.props(managerPath,
+                                  ClusterSingletonProxySettings(system)),
+      proxyName
+    )
+
+  case object PrintStatus
+}
+
+class ClusterStatusActor extends Actor {
+
+  val members = mutable.Set[Member]()
+  var leaderAddress: Option[Address] = None
+
+  override def preStart(): Unit = {
+    super.preStart()
+    Cluster(context.system)
+      .subscribe(self, InitialStateAsEvents, classOf[ClusterDomainEvent])
+  }
+
+  override def postStop(): Unit = {
+    Cluster(context.system).unsubscribe(self)
+    super.postStop()
+  }
+
+  override def receive = serviceMessageProcessor orElse clusterMessagesProcessor
+
+  private def serviceMessageProcessor: PartialFunction[Any, Unit] = {
+    case PrintStatus => sender() ! ClusterStatus(leaderAddress, members.toSet)
+  }
+
+  private def clusterMessagesProcessor: PartialFunction[Any, Unit] = {
+    case MemberUp(m)      => members += m
+    case LeaderChanged(a) => leaderAddress = a
+
+//    case _ => ???  TODO: how this should be handled!!! Somehow globally, not sure how though
+  }
+}
